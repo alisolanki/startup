86448d7b18cdb9177b7acd17c3a3d03af993c556
Ali Solanki <alisolanki123@gmail.com>
2020-03-27 03:30:16 +0530

    UserProfile 2.3

--- startup/startup/accounts/models.py
+++ startup/startup/accounts/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    img = models.ImageField(upload_to = 'pics/accounts')
     name = models.CharField(max_length = 50)
     phonenumber = models.CharField(max_length = 13)
 
--- startup/startup/user/views.py
+++ startup/startup/user/views.py
@@ -1,9 +1,31 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from accounts.models import UserProfile
 
 # Create your views here.
 def user(request, slug):
-    user = User.objects.get(username = slug)
-    userprofile = UserProfile.objects.get(user = user)
-    return render(request, 'user.html', {'userprofile' : userprofile,'user': user})
+    if request == 'POST':
+        name = request.POST['name']
+        phonenumber = request.POST['phonenumber']
+        email = request.POST['email']
+        username = request.POST['username']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+        if password1==password2:
+            if User.objects.filter(username=username).exists():
+                messages.info(request,'Username already taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request,'Email already taken')
+            else:    
+                user = User.objects.create_user(username=username, password=password1, first_name=name, email=email)
+                user = User.objects.get(username=username)
+                profile = UserProfile(user=user, name=name, phonenumber=phonenumber)
+
+                profile.save()
+                user.save()
+    else:
+        user = User.objects.get(username = slug)
+        userprofile = UserProfile.objects.get(user = user)
+        return render(request, 'user.html', {'userprofile' : userprofile,'user': user})
